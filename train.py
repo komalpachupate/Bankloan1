03fba2e6415204c491a6cfdbc2420a271559ad7c
komal pachupate <77843383+komalpachupate@users.noreply.github.com>
2021-04-04 18:20:03 +0530

    Delete train.py

--- train.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
-import pickle
-from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, roc_auc_score
-print('t')
