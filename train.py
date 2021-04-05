de7a5f34e24fbb18d4b95ecbf9388a98f870eac5
komal pachupate <77843383+komalpachupate@users.noreply.github.com>
2021-04-05 10:01:41 +0530

    Delete train.py

--- train.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-import pickle
-from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, roc_auc_score
-print('t')
